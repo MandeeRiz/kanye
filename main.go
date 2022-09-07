0ad232db72c0a620601dfca968330383193f3c54
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-09-07 12:07:07 -0400

    Finished API project

--- main.go
+++ main.go
@@ -6,9 +6,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+// func Quote(context *gin.Context)  {
+// 	context.IndentedJSON(http.StatusOK, responseData)
+// }
+
+func Quote() string {
 	response, err := http.Get("https://api.kanye.rest")
 
 	if err != nil {
@@ -20,6 +25,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf(string(responseData) + " " + "Amanda&Kalese")
+	resp := string (responseData)
+	ourNames := resp + " Amanda & Kalese"
+	return ourNames
+
+	// fmt.Printf(string(responseData) + " " + "Amanda&Kalese")
+
+	// router := gin.Default() 
+	// router.Run("localhost:9090")
 
 }
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+	  c.JSON(http.StatusOK, gin.H{
+		"message": Quote(),
+	  })
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+  }
